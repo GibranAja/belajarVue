2eaff2e3f595199f8c7f326c920a4a6336c17c1d
GibranAja <mgibranarrffi@gmail.com>
2024-06-07 23:14:37 +0700

    fix: fixed nested routes and Multi Layouting

---
+++ src/layouts/PublicLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <ul>
+            <li>
+                List 1
+            </li>
+            <li>
+                List 2
+            </li>
+            <li>
+                List 3
+            </li>
+        </ul>
+    </header>
+    <main>
+        <RouterView />
+    </main>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,58 @@ import CategoryView from '../views/CategoryView.vue'
 import NewsView from '../views/NewsView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import DetailCategory from '../views/DetailCategory.vue'
+import LandingPageView from '../views/LandingPageView.vue'
+import DashboardLayout from '../layouts/DashboardLayout.vue'
+import PublicLayout from '../layouts/PublicLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // dashboard
+    {
+      path: '/dashboard',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: DashBoardView
+        },
+        {
+          path: 'news',
+          name: 'News',
+          component: NewsView
+        },
+        {
+          path: 'category',
+          name: 'Category',
+          component: CategoryView
+        },
+        {
+          path: 'user',
+          name: 'User',
+          component: ProfileView
+        },
+        {
+          path: 'category/:name',
+          name: 'Category-name',
+          component: DetailCategory
+        }
+      ]
+    },
+    
+    // Public
     {
       path: '/',
-      name: 'Home',
-      component: DashBoardView
+      component: PublicLayout,
+      children: [
+        {
+          path: '/',
+          name: LandingPageView,
+          component: LandingPageView
+        }
+      ]
     },
-    {
-      path: '/news',
-      name: 'News',
-      component: NewsView
-    },
-    {
-      path: '/category',
-      name: 'Category',
-      component: CategoryView
-    },
-    {
-      path: '/user',
-      name: 'User',
-      component: ProfileView
-    },
-    {
-      path: '/category/:name',
-      name: 'Category-name',
-      component: DetailCategory
-    }
   ]
 })
 
