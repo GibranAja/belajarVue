a5e120e41a00dc628b4ff73370eecff6b91d2792
GibranAja <mgibranarrffi@gmail.com>
2024-06-07 22:52:21 +0700

    add: IconLink for profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,4 +2,12 @@
     <div>
         <h1>Ini Halaman Profile</h1>
     </div>
+    <v-divider></v-divider>
+    <IconLink color="red" icon="mdi-youtube" link="https://www.youtube.com" />
+    <IconLink color="black" icon="mdi-github" link="https://github.com/GibranAja" />
+    <IconLink color="pink" icon="mdi-instagram" link="https://instagram.com" />
 </template>
+
+<script setup>
+import IconLink from '../components/IconLink.vue'
+</script>
