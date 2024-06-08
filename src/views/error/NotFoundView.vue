a2219c0afb67b79f854d929eeb888d6c8a86cd8a
GibranAja <mgibranarrffi@gmail.com>
2024-06-09 06:08:46 +0700

    feat: add error and public page views

---
+++ src/views/error/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="d-flex justify-center">
+        <div>
+            <img :src="NotFound" style="width: 40vw;" alt="">
+            <h1>Nyari apa sih sayang</h1>
+            <v-btn color="green" class="my-10" block :to="{name: 'Home'}">Back</v-btn>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import NotFound from '../../assets/NotFound.svg'
+</script>
