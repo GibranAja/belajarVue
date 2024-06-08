40751eb51740fa9ca51bd458a106a725e2cc46e3
GibranAja <mgibranarrffi@gmail.com>
2024-06-09 06:12:44 +0700

    feat: update profile view with new link

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -4,8 +4,8 @@
     </div>
     <v-divider></v-divider>
     <IconLink color="red" icon="mdi-youtube" link="https://www.youtube.com" />
-    <IconLink color="black" icon="mdi-github" link="https://github.com/GibranAja" />
-    <IconLink color="pink" icon="mdi-instagram" link="https://instagram.com" />
+    <IconLink color="black" icon="mdi-github" link="https://github.com/GibranAja/belajarVue" />
+    <IconLink color="pink" icon="mdi-instagram" link="https://www.instagram.com/p/C7l72iBBuI4/" />
 </template>
 
 <script setup>
