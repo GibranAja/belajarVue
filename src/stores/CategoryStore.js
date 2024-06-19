218938c740c8b1bfacc3794ef51b9d2256838825
GibranAja <mgibranarrffi@gmail.com>
2024-06-19 17:34:15 +0700

    feat: update category feature

--- src/stores/CategoryStore.js
+++ src/stores/CategoryStore.js
@@ -1,30 +1,55 @@
 import { defineStore } from 'pinia'
 import { ref, reactive } from 'vue'
-import { addDoc, collection, getDocs } from 'firebase/firestore'
+import { addDoc, collection, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 import { db } from '../config/firebase'
 
 export const useCategoryStore = defineStore('Category', () => {
   const CategoryCollection = collection(db, 'categories')
 
   const dialog = ref(false)
+  const dialogDetail = ref(false)
+  const dialogDelete = ref(false)
   const form = ref(false)
   const category = reactive({
+    id: '',
     name: '',
-    description: ''
+    description: '',
+    isUpdate: false
   })
 
   const categories = ref(null)
 
   const onSubmitData = async () => {
     if (!form.value) return
-    await addDoc(CategoryCollection, {
-      name: category.name,
-      description: category.description
-    })
+
+    if (category.isUpdate) {
+      await updateDoc(doc(CategoryCollection, category.id), {
+        name: category.name,
+        description: category.description
+      })
+    } else {
+      await addDoc(CategoryCollection, {
+        name: category.name,
+        description: category.description
+      })
+    }
     alert('Success')
     dialog.value = false
 
     readCategory()
+  }
+
+  const getIdData = (item) => {
+    category.id = item.id
+    category.name = item.name
+    category.description = item.description
+  }
+
+  const getData = (item) => {
+    dialogDetail.value = true
+    category.id = item.id
+    category.name = item.name
+    category.description = item.description
   }
 
   const readCategory = async () => {
@@ -34,5 +59,50 @@ export const useCategoryStore = defineStore('Category', () => {
     })
   }
 
-  return { dialog, form, category, onSubmitData, readCategory, categories }
+  const clearInput = () => {
+    category.id = ''
+    category.name = ''
+    category.description = ''
+    category.isUpdate = false
+  }
+
+  const addData = () => {
+    dialog.value = true
+    clearInput()
+  }
+
+  const editData = (item) => {
+    dialog.value = true
+    getIdData(item)
+    category.isUpdate = true
+  }
+
+  const deleteData = (item) => {
+    dialogDelete.value = true
+    getIdData(item)
+  }
+
+  const destroyData = async (itemId) => {
+    await deleteDoc(doc(CategoryCollection, itemId))
+    alert('Delete Success')
+    dialogDelete.value = false
+    clearInput()
+    readCategory()
+  }
+
+  return {
+    dialog,
+    dialogDetail,
+    dialogDelete,
+    form,
+    category,
+    onSubmitData,
+    readCategory,
+    categories,
+    getData,
+    addData,
+    editData,
+    deleteData,
+    destroyData
+  }
 })
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,17 +1,43 @@
 <template>
   <h1>Ini Halaman Category</h1>
   <div class="d-flex justify-end my-6">
-    <v-btn color="primary" icon="mdi-plus" size="large" @click="dialog = true"></v-btn>
+    <v-btn color="primary" icon="mdi-plus" size="large" @click="addData()"></v-btn>
   </div>
 
   <DialogComponents v-model="dialog">
-    <template #title> Add Category </template>
+    <template #title> 
+      <div>
+        {{ category.isUpdate ? "Update Category" : "Add Category" }}
+      </div>  
+    </template>
     <template #content>
       <v-form v-model="form" @submit.prevent="onSubmitData">
         <v-text-field class="mb-1" :rules="nameRules" v-model="category.name" label="Name"></v-text-field>
         <v-textarea class="mb-1" :rules="descriptionRules" v-model="category.description" label="Description"></v-textarea>
-        <v-btn block :disabled="!form" color="success" size="large" type="submit" variant="elevated">Add</v-btn>
+        <v-btn block :disabled="!form" color="success" size="large" type="submit" variant="elevated">{{ category.description ? "Update" : "Add" }}</v-btn>
       </v-form>
+    </template>
+  </DialogComponents>
+
+  <DialogComponents v-model="dialogDetail">
+    <template #title>{{ category.name }}</template>
+    <template #content>{{ category.description }}</template>
+  </DialogComponents>
+
+  <DialogComponents v-model="dialogDelete">
+    <template #title>
+      Delete Category
+    </template>
+    <template #content>
+      <div class="text-h6 text-error pa-3">Are you sure want to delete {{ category.name }}</div>
+      <v-row>
+        <v-col>
+          <v-btn color="primary" block @click="destroyData(category.id)">Yes</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="error" block @click="dialogDelete = false">No</v-btn>
+        </v-col>
+      </v-row>
     </template>
   </DialogComponents>
 
@@ -29,9 +55,9 @@
         <td>{{ index + 1 }}</td>
         <td>{{ item.name }}</td>
         <td class="text-center">
-          <v-btn size="x-small" color="info" icon="mdi-information" />
-          <v-btn size="x-small" color="primary" icon="mdi-pencil" class="mx-3" />
-          <v-btn size="x-small" color="error" icon="mdi-trash-can" />
+          <v-btn size="x-small" color="info" icon="mdi-information" @click="getData(item)" />
+          <v-btn size="x-small" color="primary" icon="mdi-pencil" class="mx-3" @click="editData(item)" />
+          <v-btn size="x-small" color="error" icon="mdi-trash-can" @click="deleteData(item)" />
         </td>
       </tr>
     </tbody>
@@ -48,10 +74,10 @@ import { onMounted } from 'vue';
 const CategoryStorage = useCategoryStore()
 
 // State
-const { category, form, dialog, categories } = storeToRefs(CategoryStorage)
+const { category, form, dialog, dialogDetail,dialogDelete, categories } = storeToRefs(CategoryStorage)
 
 // Action
-const { onSubmitData, readCategory } = CategoryStorage
+const { onSubmitData, readCategory, getData, addData, editData, deleteData, destroyData } = CategoryStorage
 
 onMounted(() => {
   readCategory()
