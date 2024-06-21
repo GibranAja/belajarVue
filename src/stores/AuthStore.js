f4a1773b5df1b8f03780280431457b2ab91daae0
GibranAja <mgibranarraffi@gmail.com>
2024-06-21 17:47:59 +0700

    feat: current user management and logout in AuthStore

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,36 +1,69 @@
-import { auth, db } from "../config/firebase.js"
-import { defineStore } from "pinia"
-import { reactive, ref } from "vue"
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import { addDoc, collection } from "firebase/firestore"
-import { useRouter } from "vue-router"
+import { auth, db } from '../config/firebase.js'
+import { defineStore } from 'pinia'
+import { reactive, ref } from 'vue'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { addDoc, collection, query, where, getDocs } from 'firebase/firestore'
+import { useRouter } from 'vue-router'
 
 export const useAuthStore = defineStore('auth', () => {
-    const formInput =  ref(false)
-    const router = useRouter()
+  const formInput = ref(false)
+  const router = useRouter()
 
-    const userCollection = collection(db, "users")
+  const currentUser = ref(null)
 
+  const userCollection = collection(db, 'users')
 
-    const user = reactive({
-        name: '',
-        email: '',
-        password: '',
+  const user = reactive({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const userHandler = () => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const queryId = query(userCollection, where('uid', '==', user.uid))
+
+        const queryData = await getDocs(queryId)
+        const queryUser = queryData.docs[0].data()
+
+        currentUser.value = {}
+        currentUser.value.email = user.email
+        currentUser.value.id = user.uid
+        currentUser.value.name = queryUser.name
+        currentUser.value.isAdmin = queryUser.isAdmin
+      } else {
+        currentUser.value = null
+      }
     })
+  }
 
-    const authUser = async () => {
-        const data = await createUserWithEmailAndPassword(auth, user.email, user.password)
-        await addDoc(userCollection, {
-            name: user.name,
-            isAdmin: false,
-            uid: data.user.uid
-        })
-        router.push({ name: 'Home' })
-    }
+  const logOutUser = () => {
+    signOut(auth)
+      .then(() => {
+        router.push({ name: 'HomePublic' })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    return{
-        formInput,
-        user,
-        authUser,
-    }
+  const authUser = async () => {
+    const data = await createUserWithEmailAndPassword(auth, user.email, user.password)
+    await addDoc(userCollection, {
+      name: user.name,
+      isAdmin: false,
+      uid: data.user.uid
+    })
+    router.push({ name: 'Home' })
+  }
+
+  return {
+    formInput,
+    user,
+    authUser,
+    userHandler,
+    currentUser,
+    logOutUser
+  }
 })
