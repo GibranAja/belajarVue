16b72df23fa720527bf171460ddda2f252664b62
GibranAja <mgibranarraffi@gmail.com>
2024-06-25 06:14:37 +0700

    refactor: modify NewsView component

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,3 +1,6 @@
 <template>
   <h1>Ini Halaman News</h1>
+  <div class="d-flex justify-end my-6">
+    <v-btn color="primary" size="large" icon="mdi-plus" :to="{ name: 'CreateNews' }"></v-btn>
+  </div>
 </template>
