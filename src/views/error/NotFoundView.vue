241c46b4ef8723787272e512bf79c21446808d0e
GibranAja <mgibranarraffi@gmail.com>
2024-06-24 22:48:41 +0700

    refactor: revise error message

--- src/views/error/NotFoundView.vue
+++ src/views/error/NotFoundView.vue
@@ -2,7 +2,7 @@
     <div class="d-flex justify-center">
         <div>
             <img :src="NotFound" style="width: 40vw;" alt="">
-            <h1>Nyari apa sih sayang</h1>
+            <h1>Halaman yang kamu cari tidak di temukan</h1>
             <v-btn color="green" class="my-10" block :to="{name: 'Home'}">Back</v-btn>
         </div>
     </div>
