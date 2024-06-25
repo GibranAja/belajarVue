5ec0dfe134b589167c9b98ca1783a85bac40fb07
GibranAja <mgibranarraffi@gmail.com>
2024-06-26 05:27:54 +0700

    fix: set ref to false

--- src/stores/NewsStore.js
+++ src/stores/NewsStore.js
@@ -6,55 +6,55 @@ import { useRouter } from 'vue-router'
 import { collection, addDoc } from 'firebase/firestore'
 
 export const useNewsStore = defineStore('News', () => {
-     const news = reactive({
-        id: "",
-        title: "",
-        content: "",
-        category: "",
-        isUpdate: false
-     })
+  const news = reactive({
+    id: '',
+    title: '',
+    content: '',
+    category: '',
+    isUpdate: false
+  })
 
-     const formInput = ref
+  const formInput = ref(false)
 
-    //  Router
-    const router = useRouter()
+  //  Router
+  const router = useRouter()
 
-    // Storage
-    const authStore = useAuthStore()
+  // Storage
+  const authStore = useAuthStore()
 
-    // Collection
-    const newsCollection = collection(db, "news")
+  // Collection
+  const newsCollection = collection(db, 'news')
 
-    const handleSubmit = async () => {
-        if(news.isUpdate) {
-            // Update Data
-        } else {
-            // Add Data
-            const now = Date.now();
-            await addDoc(newsCollection, {
-                title: news.title,
-                content: news.content,
-                category: {
-                    id: news.category.id,
-                    name: news.category.name
-                },
-                isApprove: false,
-                image: null,
-                writtenBy: {
-                    id: authStore.currentUser.id,
-                    name: authStore.currentUser.name,
-                    email: authStore.currentUser.email,
-                },
-                createdAt: now
-            })
-            alert("Succes adding news document")
-        }
-        router.push({ name: "News"})
-     }
+  const handleSubmit = async () => {
+    if (news.isUpdate) {
+      // Update Data
+    } else {
+      // Add Data
+      const now = Date.now()
+      await addDoc(newsCollection, {
+        title: news.title,
+        content: news.content,
+        category: {
+          id: news.category.id,
+          name: news.category.name
+        },
+        isApprove: false,
+        image: null,
+        writtenBy: {
+          id: authStore.currentUser.id,
+          name: authStore.currentUser.name,
+          email: authStore.currentUser.email
+        },
+        createdAt: now
+      })
+      alert('Succes adding news document')
+    }
+    router.push({ name: 'News' })
+  }
 
-     return{
-        news,
-        formInput,
-        handleSubmit
-     }
+  return {
+    news,
+    formInput,
+    handleSubmit
+  }
 })
