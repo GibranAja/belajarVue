3e3028bf3dc4a84666f17b7b81096ae0d10af8c0
GibranAja <mgibranarraffi@gmail.com>
2024-06-26 06:03:07 +0700

    feat(read): display newsData from Firebase storage

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,50 @@
 <template>
   <h1>Ini Halaman News</h1>
+  <!-- Add button -->
   <div class="d-flex justify-end my-6">
     <v-btn color="primary" size="large" icon="mdi-plus" :to="{ name: 'CreateNews' }"></v-btn>
   </div>
+  <v-row justify="center">
+    <v-col v-for="data in newsData" :key="data.id" cols="4">
+      <v-card>
+        <v-img
+          class="align-end text-white"
+          height="300"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          cover
+        >
+          <v-card-title>{{ data.title.substring(0, 50) }}</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pt-4"> {{ data.category.name }} </v-card-subtitle>
+
+        <v-card-text>
+          <div>{{ data.content.substring(0, 200) }}</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="orange">Detail</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+<script setup>
+import { useNewsStore } from '@/stores/NewsStore'
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+
+// Store
+const newsStore = useNewsStore()
+
+// State
+const { newsData } = storeToRefs(newsStore)
+
+// Action
+const { allNews } = newsStore
+
+onMounted(() => {
+  allNews()
+})
+</script>
