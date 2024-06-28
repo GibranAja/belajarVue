e330bc4aaf096f67f174e274822e2b9d8df18f0d
GibranAja <mgibranarraffi@gmail.com>
2024-06-28 09:14:34 +0700

    feat: create UpdateNewsView component

---
+++ src/views/news/UpdateNewsView.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Update News</h1>
+    <FormInputNews />
+</template>
+
+<script setup>
+import FormInputNews from '../../components/news/FormInputNews.vue'
+import { useRoute } from 'vue-router'
+import { useNewsStore } from '../../stores/NewsStore'
+import { onMounted } from 'vue'
+
+// Store
+const newsStore = useNewsStore()
+
+// Route
+const newsRoute = useRoute()
+
+// Action
+const { updateHandling } = newsStore
+
+onMounted(() => {
+    updateHandling(newsRoute.params.id)
+})
+</script>
