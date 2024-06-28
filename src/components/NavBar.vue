75aaf1b558a12cfb201c645bccdc11523aa2c5d8
GibranAja <mgibranarraffi@gmail.com>
2024-06-28 19:24:15 +0700

    refactor: update app bar title from 'Admin Portal Berita' to 'Admin HNN'

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <v-app-bar color="blue-lighten-4" prominent>
     <v-app-bar-nav-icon variant="text" @click.stop="$emit('openClose')"></v-app-bar-nav-icon>
 
-    <v-toolbar-title>Admin Portal Berita </v-toolbar-title>
+    <v-toolbar-title>Admin HNN</v-toolbar-title>
 
     <v-spacer></v-spacer>
     <v-btn color="red" icon="mdi-logout" variant="text" @click="logOutUser"></v-btn>
