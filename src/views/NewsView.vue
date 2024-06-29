f5a7a8bce460c78616a2f79394659ccc65b0e175
GibranAja <mgibranarraffi@gmail.com>
2024-06-29 08:18:02 +0700

    fix: fixed error when deleting news

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -99,6 +99,7 @@ onMounted(() => {
 })
 
 const truncateText = (text, maxLength) => {
+  if (!text || typeof text !== 'string') return ''
   if (text.length <= maxLength) return text
   return text.slice(0, maxLength) + '...'
 }
