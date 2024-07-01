fcad4259cf6dccabede8129d85fdb578e616ab7b
GibranAja <mgibranarraffi@gmail.com>
2024-07-02 06:46:57 +0700

    feat: add loading animation when no one is Login

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!currentUser">
-    <h1>Loading...</h1>
+    <div class="loader"></div>
   </div>
   <div v-else>
     <h1>Halo {{ currentUser.name }}👋</h1>
@@ -9,10 +9,41 @@
 </template>
 
 <script setup>
-import { useAuthStore } from '@/stores/AuthStore';
-import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores/AuthStore'
+import { storeToRefs } from 'pinia'
 
 // Store
 const auth = useAuthStore()
 const { currentUser } = storeToRefs(auth)
 </script>
+
+<style scoped>
+/* HTML: <div class="loader"></div> */
+.loader {
+  width: 50px;
+  aspect-ratio: 1;
+  display: grid;
+  border: 4px solid #0000;
+  border-radius: 50%;
+  border-right-color: #25b09b;
+  animation: l15 1s infinite linear;
+}
+.loader::before,
+.loader::after {
+  content: '';
+  grid-area: 1/1;
+  margin: 2px;
+  border: inherit;
+  border-radius: 50%;
+  animation: l15 2s infinite;
+}
+.loader::after {
+  margin: 8px;
+  animation-duration: 3s;
+}
+@keyframes l15 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
