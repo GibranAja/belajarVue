65e90ff67a2d27967845febfbb816bc4383db8ef
GibranAja <mgibranarraffi@gmail.com>
2024-07-01 19:40:26 +0700

    feat: change link youtube in profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -8,7 +8,7 @@
         </div>
     </div>
     <v-divider></v-divider>
-    <IconLink color="red" icon="mdi-youtube" link="https://www.youtube.com" />
+    <IconLink color="red" icon="mdi-youtube" link="https://www.youtube.com/watch?v=lLJGB2fcZxY" />
     <IconLink color="black" icon="mdi-github" link="https://github.com/GibranAja/belajarVue" />
     <IconLink color="pink" icon="mdi-instagram" link="https://www.instagram.com/p/C7l72iBBuI4/" />
 </template>
