5afcad35a998d43c75e962ff6e829fe55bc4055b
GibranAja <mgibranarraffi@gmail.com>
2024-07-01 12:57:57 +0700

    feat: add getStorage function from firebase/storage

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,28 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {getFirestore} from "firebase/firestore"
-import {getAuth} from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAR_irCnBtZXLM9UlzJ7dpJGeFDydOfP0",
-  authDomain: "portalberita-404c6.firebaseapp.com",
-  projectId: "portalberita-404c6",
-  storageBucket: "portalberita-404c6.appspot.com",
-  messagingSenderId: "747817298351",
-  appId: "1:747817298351:web:3144208de022ccb8d9a75a"
-};
+  apiKey: 'AIzaSyDAR_irCnBtZXLM9UlzJ7dpJGeFDydOfP0',
+  authDomain: 'portalberita-404c6.firebaseapp.com',
+  projectId: 'portalberita-404c6',
+  storageBucket: 'portalberita-404c6.appspot.com',
+  messagingSenderId: '747817298351',
+  appId: '1:747817298351:web:3144208de022ccb8d9a75a'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
 // Database
 const db = getFirestore(app)
 const auth = getAuth(app)
+const projectStorage = getStorage(app)
 
-export {
-    db,
-    auth
-}
+export { db, auth, projectStorage }
