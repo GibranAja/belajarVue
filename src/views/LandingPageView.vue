ddb94a7301b3b8d6042276c96a987250e9ec10b6
GibranAja <mgibranarraffi@gmail.com>
2024-07-02 14:42:40 +0700

    feat: make the news to public

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>Ini Landing Page</h1>
+  <h1 class="my-3">News</h1>
+  <v-divider class="border-opacity-100" color="info"></v-divider>
+  <v-row class="my-3">
+    <v-col col="2" v-for="data in newsData" :key="data.id">
+      <v-card class="mx-auto">
+        <v-img
+          class="align-end text-white"
+          height="400"
+          :src="data.image ? data.image : `https://cdn.vuetifyjs.com/images/cards/docks.jpg`"
+          cover
+        >
+        </v-img>
+
+        <v-card-title>{{ truncateText(data.title, 100) }}</v-card-title>
+        <v-card-subtitle class="pt-4"> {{ data.category.name }} </v-card-subtitle>
+
+        <v-card-text>
+          <div>{{ truncateText(data.content, 200) }}</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="info" variant="elevated" type="button" class="mb-3 ml-2">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+<script setup>
+import { useNewsStore } from '../stores/NewsStore.js'
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+
+// Store
+const newsStore = useNewsStore()
+
+// Action
+const { allNews } = newsStore
+
+// State
+const { newsData } = storeToRefs(newsStore)
+
+const truncateText = (text, maxLength) => {
+  if (!text || typeof text !== 'string') return ''
+  if (text.length <= maxLength) return text
+  return text.slice(0, maxLength) + '...'
+}
+
+onMounted(() => {
+  allNews()
+})
+</script>
