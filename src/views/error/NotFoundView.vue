ae618bd5b55db5055e075502f04241f0778b622e
GibranAja <mgibranarraffi@gmail.com>
2024-07-02 17:17:39 +0700

    feat: change router when click button

--- src/views/error/NotFoundView.vue
+++ src/views/error/NotFoundView.vue
@@ -3,7 +3,7 @@
         <div>
             <img :src="NotFound" style="width: 40vw;" alt="">
             <h1>Halaman yang kamu cari tidak di temukan</h1>
-            <v-btn color="green" class="my-10" block :to="{name: 'Home'}">Back</v-btn>
+            <v-btn color="green" class="my-10" block :to="{name: 'HomePublic'}">Back</v-btn>
         </div>
     </div>
 </template>
