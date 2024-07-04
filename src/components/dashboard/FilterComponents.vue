b0093a8b4362bdc8e4b9ff62e8e46d10348c8ba6
GibranAja <mgibranarraffi@gmail.com>
2024-07-04 11:35:28 +0700

    feat: make reusable components for filtering data by alphabet

---
+++ src/components/dashboard/FilterComponents.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn color="primary" class="mr-3 mt-3" @click="toggleSortOrder">
+    Filter {{ sortOrder === 'asc' ? 'A-Z' : 'Z-A' }}
+  </v-btn>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// Filter Data
+const sortOrder = ref('asc')
+
+const toggleSortOrder = () => {
+  sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc'
+}
+</script>
