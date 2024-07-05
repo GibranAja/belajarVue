1cda6155fd5bbb4e02d26b75c3401e3cc62557cd
GibranAja <mgibranarraffi@gmail.com>
2024-07-05 10:23:08 +0700

    refactor: update logout process to use confirmation dialog

--- src/components/public/HeaderComponent.vue
+++ src/components/public/HeaderComponent.vue
@@ -11,24 +11,45 @@
       </v-btn>
     </v-toolbar-items>
     <v-toolbar-items v-else>
-      <v-btn flat v-for="item in currentUserMenu" :key="item.title" :to="{ name: `${item.pathName}` }">
+      <v-btn
+        flat
+        v-for="item in currentUserMenu"
+        :key="item.title"
+        :to="{ name: `${item.pathName}` }"
+      >
         <v-icon left>{{ item.icon }}</v-icon>
         <span>{{ item.title }}</span>
       </v-btn>
-      <v-btn color="red" icon="mdi-logout" variant="text" @click="logOutUser"></v-btn>
+      <v-btn color="red" icon="mdi-logout" variant="text" @click="auth.confirmLogout"></v-btn>
     </v-toolbar-items>
   </v-toolbar>
+
+  <DialogComponents v-model="auth.dialogLogout">
+    <template #title>Logout Confirmation</template>
+    <template #content>
+      <div class="text-h6 pa-3">Are you sure you want to logout?</div>
+      <v-row>
+        <v-col>
+          <v-btn color="error" block @click="auth.logOutUser">Yes</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" block @click="auth.dialogLogout = false">No</v-btn>
+        </v-col>
+      </v-row>
+    </template>
+  </DialogComponents>
 </template>
 
 <script setup>
 import { inject, computed } from 'vue'
-import { useAuthStore } from '@/stores/AuthStore';
+import { useAuthStore } from '@/stores/AuthStore'
+import DialogComponents from '../dashboard/DialogComponents.vue'
 
 defineEmits(['openClose'])
 
 // Store
 const auth = useAuthStore()
-const { logOutUser } = auth
+// const { logOutUser } = auth
 
 const currentUser = inject('currentUser')
 
