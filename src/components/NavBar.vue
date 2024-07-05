27b6ddd5a4960fa34a3358a81cab5d13ce33abdf
GibranAja <mgibranarraffi@gmail.com>
2024-07-05 13:19:59 +0700

    feat(logout): implement logout confirmation dialog

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,14 +7,30 @@
     <v-spacer></v-spacer>
     <v-btn color="red" icon="mdi-logout" variant="text" @click="logOutUser"></v-btn>
   </v-app-bar>
+
+  <DialogComponents v-model="auth.dialogLogout">
+    <template #title>Logout Confirmation</template>
+    <template #content>
+      <div class="text-h6 pa-3">Are you sure you want to logout?</div>
+      <v-row>
+        <v-col>
+          <v-btn color="error" block @click="auth.logOutUser">Yes</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" block @click="auth.dialogLogout = false">No</v-btn>
+        </v-col>
+      </v-row>
+    </template>
+  </DialogComponents>
 </template>
 
 <script setup>
-import { useAuthStore } from '@/stores/AuthStore';
+import { useAuthStore } from '@/stores/AuthStore'
+import DialogComponents from './dashboard/DialogComponents.vue'
 
 defineEmits(['openClose'])
 
 // Store
 const auth = useAuthStore()
-const { logOutUser } = auth
+// const { logOutUser } = auth
 </script>
