94ae1b0fa010f1597d413fa7c5bfc780a1c1dbf2
GibranAja <mgibranarraffi@gmail.com>
2024-07-07 20:03:30 +0700

    feat: implement searchBar component

--- src/components/public/HeaderComponent.vue
+++ src/components/public/HeaderComponent.vue
@@ -4,6 +4,12 @@
       <span class="ml-15 text-h5 font-weight-black">HNN</span>
     </RouterLink>
     <v-spacer></v-spacer>
+    <SearchBar
+      v-model:searchQuery="searchQuery"
+      label="Search News..."
+      @search="searchNews"
+    />
+
     <v-toolbar-items v-if="!currentUser">
       <v-btn flat v-for="item in menuItems" :key="item.title" :to="{ name: `${item.pathName}` }">
         <v-icon left>{{ item.icon }}</v-icon>
@@ -24,6 +30,20 @@
     </v-toolbar-items>
   </v-toolbar>
 
+  <v-container v-if="searchPerformed">
+    <h2>Search Results</h2>
+    <v-row v-if="filteredNews.length > 0">
+      <v-col v-for="item in filteredNews" :key="item.id" cols="12" md="4">
+        <v-card>
+          <v-card-title>{{ item.title }}</v-card-title>
+          <v-card-subtitle>{{ item.category.name }}</v-card-subtitle>
+          <v-card-text>{{ item.content }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <p v-else>No results found.</p>
+  </v-container>
+
   <DialogComponents v-model="auth.dialogLogout">
     <template #title>Logout Confirmation</template>
     <template #content>
@@ -41,15 +61,32 @@
 </template>
 
 <script setup>
-import { inject, computed } from 'vue'
+import { ref, inject, computed, onMounted } from 'vue'
 import { useAuthStore } from '@/stores/AuthStore'
+import { useNewsStore } from '@/stores/NewsStore'
 import DialogComponents from '../dashboard/DialogComponents.vue'
+import SearchBar from '../SearchBar.vue'
 
 defineEmits(['openClose'])
 
 // Store
 const auth = useAuthStore()
-// const { logOutUser } = auth
+const newsStore = useNewsStore()
+
+// SearchBar
+const searchQuery = ref('')
+const searchPerformed = ref(false)
+const filteredNews = ref([])
+
+const searchNews = () => {
+  console.log('Searching for:', searchQuery.value)
+  searchPerformed.value = true
+  filteredNews.value = newsStore.newsData.filter(news => 
+    news.title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+    news.category.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+  console.log('Filtered news:', filteredNews.value)
+}
 
 const currentUser = inject('currentUser')
 
@@ -89,6 +126,12 @@ const menuUser = [
 
 const currentUserMenu = computed(() => {
   return currentUser.value?.isAdmin ? menuAdmin : menuUser
+})
+
+onMounted(async () => {
+  if (!newsStore.newsData.length) {
+    await newsStore.fetchNews()
+  }
 })
 </script>
 
