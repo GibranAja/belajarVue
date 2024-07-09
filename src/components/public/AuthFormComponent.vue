ce6059f3482b0150cb5c6d594690b53e60c93e0f
GibranAja <mgibranarraffi@gmail.com>
2024-07-09 07:45:46 +0700

    feat: make the header and footer responsive

--- src/components/public/AuthFormComponent.vue
+++ src/components/public/AuthFormComponent.vue
@@ -1,85 +1,102 @@
 <template>
-  <div>
-    <div class="mx-auto my-6 text-center text-h3 text-primary">
-      {{ isLogin ? 'Login' : 'Register' }}
-    </div>
-
-    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-      <v-form v-model="formInput" @submit.prevent="isLogin ? authUser(true) : authUser()">
-        <v-alert v-if="isError" type="error" title="Error" :text="message" variant="tonal"></v-alert>
-        <div v-if="!isLogin">
-          <div class="text-subtitle-1 text-medium-emphasis">Name</div>
-          <v-text-field
-            density="compact"
-            placeholder="Username"
-            prepend-inner-icon="mdi-account"
-            variant="outlined"
-            :rules="nameRules"
-            v-model="user.name"
-          ></v-text-field>
-        </div>
-
-        <div class="text-subtitle-1 text-medium-emphasis">Email</div>
-        <v-text-field
-          density="compact"
-          placeholder="Email address"
-          prepend-inner-icon="mdi-email-outline"
-          variant="outlined"
-          :rules="emailRules"
-          v-model="user.email"
-        ></v-text-field>
-
-        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-          Password
-        </div>
-
-        <v-text-field
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          @click:append-inner="visible = !visible"
-          :rules="[passwordRules.required, passwordRules.min]"
-          v-model="user.password"
-        ></v-text-field>
-
-        <v-btn
-          :disabled="!formInput"
-          class="mb-8"
-          type="submit"
-          color="blue"
-          size="large"
-          variant="tonal"
-          block
-        >
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6" lg="4">
+        <div class="text-center text-h3 text-primary my-6">
           {{ isLogin ? 'Login' : 'Register' }}
-        </v-btn>
-      </v-form>
+        </div>
 
-      <v-card-text class="text-center" v-if="isLogin">
-        <router-link
-          class="text-blue text-decoration-none"
-          :to="{ name: 'Register' }"
-          rel="noopener
-          noreferrer"
-        >
-          Don't have account? Sign Up Now <v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
+        <v-card class="pa-6 pb-8" elevation="8" rounded="lg">
+          <v-form v-model="formInput" @submit.prevent="isLogin ? authUser(true) : authUser()">
+            <v-alert
+              v-if="isError"
+              type="error"
+              title="Error"
+              :text="message"
+              variant="tonal"
+              class="mb-6"
+            ></v-alert>
 
-      <v-card-text class="text-center" v-else>
-        <router-link
-          class="text-blue text-decoration-none"
-          :to="{ name: 'Login' }"
-          rel="noopener noreferrer"
-        >
-          Login Now <v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
-    </v-card>
-  </div>
+            <div v-if="!isLogin" class="mb-6">
+              <div class="text-subtitle-1 text-medium-emphasis mb-2">Name</div>
+              <v-text-field
+                density="comfortable"
+                placeholder="Username"
+                prepend-inner-icon="mdi-account"
+                variant="outlined"
+                :rules="nameRules"
+                v-model="user.name"
+              ></v-text-field>
+            </div>
+
+            <div class="mb-6">
+              <div class="text-subtitle-1 text-medium-emphasis mb-2">Email</div>
+              <v-text-field
+                density="comfortable"
+                placeholder="Email address"
+                prepend-inner-icon="mdi-email-outline"
+                variant="outlined"
+                :rules="emailRules"
+                v-model="user.email"
+              ></v-text-field>
+            </div>
+
+            <div class="mb-6">
+              <div
+                class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between mb-2"
+              >
+                Password
+              </div>
+              <v-text-field
+                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'"
+                density="comfortable"
+                placeholder="Enter your password"
+                prepend-inner-icon="mdi-lock-outline"
+                variant="outlined"
+                @click:append-inner="visible = !visible"
+                :rules="[passwordRules.required, passwordRules.min]"
+                v-model="user.password"
+              ></v-text-field>
+            </div>
+
+            <v-btn
+              :disabled="!formInput"
+              class="mb-6"
+              type="submit"
+              color="blue"
+              size="large"
+              variant="tonal"
+              block
+              height="56"
+            >
+              {{ isLogin ? 'Login' : 'Register' }}
+            </v-btn>
+          </v-form>
+
+          <v-card-text class="text-center mt-4" v-if="isLogin">
+            <router-link
+              class="text-blue text-decoration-none"
+              :to="{ name: 'Register' }"
+              rel="noopener noreferrer"
+            >
+              Don't have account? Sign Up Now <v-icon icon="mdi-chevron-right"></v-icon>
+            </router-link>
+          </v-card-text>
+
+          <v-card-text class="text-center mt-4" v-else>
+            <router-link
+              class="text-blue text-decoration-none"
+              :to="{ name: 'Login' }"
+              rel="noopener noreferrer"
+            >
+              Login Now <v-icon icon="mdi-chevron-right"></v-icon>
+            </router-link>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
--- src/components/public/FooterComponent.vue
+++ src/components/public/FooterComponent.vue
@@ -1,9 +1,11 @@
 <template>
     <v-footer class="bg-blue-lighten-4">
+      <v-container fluid>
         <v-row justify="center" no-gutters>
-            <v-col class="text-center mt-4" cols="12">
-                {{ new Date().getFullYear() }} - <strong>Gibran</strong>
-            </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" class="text-center py-4">
+            {{ new Date().getFullYear() }} - <strong>Gibran</strong>
+          </v-col>
         </v-row>
+      </v-container>
     </v-footer>
-</template>
+  </template>
