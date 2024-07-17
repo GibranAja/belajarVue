53a6d8a4f8c1c71c040fb7ceeaf2b6fa68e19eb0
GibranAja <mgibranarraffi@gmail.com>
2024-07-17 19:06:02 +0700

    refactor: remove unused function

--- src/components/IconLink.vue
+++ src/components/IconLink.vue
@@ -1,20 +1,20 @@
 <template>
-    <v-btn class="ma-2" :color="color" :icon="icon" :href="link" target="#" ></v-btn>
+  <v-btn class="ma-2" :color="color" :icon="icon" :href="link" target="#"></v-btn>
 </template>
 
 <script setup>
 defineProps({
-    color: {
-        type: String,
-        required: true
-    },
-    icon: {
-        type: String,
-        required: true
-    },
-    link: {
-        type: String,
-        required: true
-    },
+  color: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  }
 })
 </script>
