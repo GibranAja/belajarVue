afb24398f324bcd8c6da36f79f355f275ca23fcf
GibranAja <mgibranarraffi@gmail.com>
2024-07-18 20:30:28 +0700

    feat: make the back button to reusable component

---
+++ src/components/news/BackButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-btn class="mt-7" color="secondary" @click="$router.go(-1)" prepend-icon="mdi-arrow-left" size="large" variant="elevate">
+    <p class="mx-3">Back</p>
+  </v-btn>
+</template>
