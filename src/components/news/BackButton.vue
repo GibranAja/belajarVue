83382df952cb3164e217c9be43a6a16e1eb6082c
GibranAja <mgibranarraffi@gmail.com>
2024-07-20 21:54:39 +0700

    style: change the tonal to elevate

--- src/components/news/BackButton.vue
+++ src/components/news/BackButton.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-btn class="mt-7" color="secondary" @click="$router.go(-1)" prepend-icon="mdi-arrow-left" size="large" variant="elevate">
+  <v-btn
+    class="mt-7"
+    color="secondary"
+    @click="$router.go(-1)"
+    prepend-icon="mdi-arrow-left"
+    size="large"
+    variant="elevate"
+  >
     <p class="mx-3">Back</p>
   </v-btn>
 </template>
